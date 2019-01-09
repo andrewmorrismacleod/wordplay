51d298c9334abae24579dd1e63d4ccc16e5e5fae
Andrew MacLeod <andrewmorrismacleod@googlemail.com>
2019-01-09 16:50:43 +0000

    Passing Anagram Solver

---
+++ hw_advanced_enumeration_start/anagram_finder/anagram_finder.js
@@ -0,0 +1,13 @@
+const AnagramFinder = function (word) {
+  this.word = word
+  this.alphabetisedWord = this.word.toLowerCase().split('').sort().join('');
+}
+
+AnagramFinder.prototype.findAnagrams = function (otherWords) {
+  return otherWords.filter((otherWord) => {
+    const alphabetisedOtherWord = otherWord.toLowerCase().split('').sort().join('');
+    return (alphabetisedOtherWord === this.alphabetisedWord) && (this.word.toLowerCase() !== otherWord.toLowerCase());
+  });
+}
+
+module.exports = AnagramFinder;
